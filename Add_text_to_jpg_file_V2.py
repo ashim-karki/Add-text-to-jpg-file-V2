cea186f732a6ab18994b78d59ea121eeb7ab4164
Ashim Karki <86644466+ashim-karki@users.noreply.github.com>
2022-02-07 21:57:26 +0545

    Update Add_text_to_jpg_file_V2.py

--- Add_text_to_jpg_file_V2.py
+++ Add_text_to_jpg_file_V2.py
@@ -26,9 +26,9 @@ for i in range(1,len(data)):
     I1 = ImageDraw.Draw(img) 
     img2 = Image.open('Images/' + data[i][0] + '.jpg') # make sure to add all images in images folder
     img2 = img2.resize((500, 500)) # resize the image to your requirement
-    I1.text((1450, 365), data[i][0], font=myFont1, fill =(255, 255, 255)) # this can't be used in a for loop
-    I1.text((1650, 465), data[i][1], font=myFont2, fill =(255, 255, 255)) 
-    I1.text((1200, 465), data[i][2], font=myFont3, fill =(255, 255, 255))
+    I1.text((1450, 365), data[i][0], font=myFont1, fill =(255, 255, 255), align='center') # this can't be used in a for loop
+    I1.text((1650, 465), data[i][1], font=myFont2, fill =(255, 255, 255), align='center') 
+    I1.text((1200, 465), data[i][2], font=myFont3, fill =(255, 255, 255), align='center')
     #I1.text((1200, 465), data[i][2], font=myFont2, fill =(255, 255, 255)) add as many texts you want in similar manner
     img.paste(img2, (130,778))
     img.save('Final/' + data[i][0] + '.jpg') # all the final templates will be saved in Final folder
